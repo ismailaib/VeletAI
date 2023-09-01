c30ee383fca78c93735884a4949ea626d4fcd990
ismailaib <ismailaitbouhmad@gmail.com>
2023-09-01 15:28:39 +0100

    setup Auth

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+
+Auth::routes();
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
